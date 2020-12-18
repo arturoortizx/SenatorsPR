75f189428b940ac4226480795e8e049b54bcf4b9
Arturo Ortiz San Miguel <65433626+arturoortizx@users.noreply.github.com>
2020-12-17 23:15:27 -0400

    Update PR_Senators.py

--- PR_Senators.py
+++ PR_Senators.py
@@ -37,96 +37,108 @@ previous = ""
 for line in f:
     line = line.removesuffix("\n")
     if "Fecha" in line:
-        senators = senators + [[previous.removeprefix("Votante : ")]]
+        senators = senators + [[[previous.removeprefix("Votante : ")]]]
     previous = line
+f.close()
+
+# adding political party to each senator:
+
+for i in range(len(senators)):
+    if i in [0, 2, 3, 6, 7, 9, 10, 12, 13, 14, 15, 16, 18, 19, 20, 21, 22, 23, 24, 28, 29]:
+        senators[i][0] = senators[i][0] + ["PNP"]
+    elif i in [1, 5, 8, 11, 17, 25, 26,]:
+        senators[i][0] = senators[i][0] + ["PPD"]
+    elif i == 4:
+        senators[i][0] = senators[i][0] + ["PIP"]
+    elif i == 27:
+        senators[i][0] = senators[i][0] + ["indep."]
+
 senators_ = senators
 print(senators_)
-f.close()
+
+
+parties = []
+for i in range(len(senators)):
+    parties = parties + [senators[i][0][1]]
 
 votes_per_sen = []
 
 # records each senators vote for each bill
-for file in range(16):
-    for n in range(len(senators)):
-        bill_num = []
-        passed_senator = False
-        f = open("PRSenate" + str(file + 1) + ".txt", "r")
-        sen_name = senators[n][0]
-        for pos, l in enumerate(f):
-            l = l.replace("\r", "").replace("\n", "")
-            if "Votante" in l and passed_senator:
-                break
+def read_txt(x, y):
+    senators = senators_
+    for file in range(x, y):
+        for n in range(len(senators)):
+            bill_num = []
+            passed_senator = False
+            f = open("PRSenate" + str(file) + ".txt", "r")
+            sen_name = senators[n][0][0]
+            for pos, l in enumerate(f):
+                l = l.replace("\r", "").replace("\n", "")
+                if "Votante" in l and passed_senator:
+                    break
 
-            if sen_name in l:
-                passed_senator = True
-            if passed_senator:
-                if l.isnumeric():
-                    bill_num.append(l)
-            if ("Ausente" in l or "A favor" in l or "En contra" in l or "Abstenido" in l) and passed_senator and bill_num:
-                senators[n].append([bill_num.pop(0), vote_to_num(l)])
-        f.close()
+                if sen_name in l:
+                    passed_senator = True
+                if passed_senator:
+                    if l.isnumeric():
+                        bill_num.append(l)
+                if ("Ausente" in l or "A favor" in l or "En contra" in l or "Abstenido" in l) and passed_senator and bill_num:
+                    senators[n].append([bill_num.pop(0), vote_to_num(l)])
+            f.close()
 
-print(senators)
-# Figuring out the numbers of the distinct bill numbers
-billNums = dict()
-billCount = 0
+    print(senators)
+    # Figuring out the numbers of the distinct bill numbers
+    billNums = dict()
+    billCount = 0
 
-for L in senators:
-    for i in range(1, len(L)):
-        Li = L[i][0]
-        if Li not in billNums:
-            billNums[Li] = billCount
-            billCount = billCount + 1
+    for L in senators:
+        for i in range(1, len(L)):
+            Li = L[i][0]
+            if Li not in billNums:
+                billNums[Li] = billCount
+                billCount = billCount + 1
 
-# Creating the matrix/table
-myData = np.zeros([30, len(billNums)])
+    # Creating the matrix/table
+    myData = np.zeros([30, len(billNums)])
 
-for i in range(30):
-    Li = senators[i]
-    for j in range(1, len(Li)):
-        Lj = Li[j]
-        jdx = billNums[Lj[0]]
-        myData[i, jdx] = Lj[1]
+    for i in range(30):
+        Li = senators[i]
+        for j in range(1, len(Li)):
+            Lj = Li[j]
+            jdx = billNums[Lj[0]]
+            myData[i, jdx] = Lj[1]
 
 
-df = pd.DataFrame(myData)
-for n in range(len(list(billNums.keys()))):
-    df = df.rename(columns={n : list(billNums.keys())[n]})
+    df = pd.DataFrame(myData)
+    for n in range(len(list(billNums.keys()))):
+        df = df.rename(columns={n : list(billNums.keys())[n]})
 
-for m in range(len(senators_)):
-    df = df.rename(index={m : senators_[m][0]})
+    for m in range(len(senators)):
+        df = df.rename(index={m : senators[m][0][0]})
 
-print(df)
+    print(df)
 
-[U, S, V] = SVD.rsvd(myData, 30, 1, 5)
-print(np.zeros(np.shape(myData)))
-error = np.zeros([np.linalg.matrix_rank(myData), 1])
 
-A = np.zeros((30, np.linalg.matrix_rank(myData)), int)
-for i in range(np.linalg.matrix_rank(myData)):
-    A = A + S[i, i] * (U[:, i].dot(V[:, i]))
+    print(np.zeros(np.shape(myData)))
 
-for k in range(np.linalg.matrix_rank(myData)):
-    Ak = np.zeros((30, np.linalg.matrix_rank(myData)), int)
-    for i in range(k):
-        Ak = Ak + S[i, i] * (U[:, i].dot(V[:, i]))
-    error[k] = np.linalg.norm(A - Ak, 2)
+    return myData
 
-print(plt.plot(error))
 
-# Making the plot
-# plt.scatter(U1, U2)
-# plt.show()
-# Or
-#plt.savefig("svdplot.png")
-#plt.close()
-#U = svd[0]
-#plt.scatter(U[:,0], U[:,1])
+def plot_err(title, d):
+    error = np.zeros([np.linalg.matrix_rank(d), 1])
+    for k in range(1, np.linalg.matrix_rank(d)):
+        [U, S, V] = SVD.rsvd(d, k, 5, 1)
+        Ak = np.zeros((30, np.linalg.matrix_rank(d)), int)
+        for i in range(k):
+            Ak = U @ np.diag(S) @ V
+        error[k - 1] = np.linalg.norm(d - Ak) / np.linalg.norm(d)
 
-# Figuring out rank
-# np.linalg.norm(myData - myData_k, 2)
-# Uk @ Sk @ Vk
-# np.linalg.norm(myData - myData_k, 2) / np.linalg.norm(myData, 2)
+    plt.plot(error)
+    plt.title(title)
+    plt.show()
 
-# plt.scatter(U1, U2) instead of plot
-# Color code the plots
+def u_ret(d):
+    r = np.linalg.matrix_rank(d)
+    [U, S, V] = SVD.rsvd(r, r, 5, 1)[0]
+    return U
+
