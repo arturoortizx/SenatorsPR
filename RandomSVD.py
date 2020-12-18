a9dd3c94b8cd1b54fa811420081e66f757df6772
Arturo Ortiz San Miguel <65433626+arturoortizx@users.noreply.github.com>
2020-12-17 23:14:06 -0400

    Update RandomSVD.py

--- RandomSVD.py
+++ RandomSVD.py
@@ -2,15 +2,14 @@ import numpy as np
 
 def rsvd(A, r, q, p):
     A = np.array(A)
-    ny = np.size(A, 1)
+    ny = A.shape[1]
     P = np.random.randn(ny, r + p)
     Z = A.dot(P)
     for k in range(q):
         Z = A.dot(np.transpose(A).dot(Z))
 
-    [Q, R] = np.linalg.qr(Z)
-    Y = np.transpose(Q).dot(Z)
-    [UY, S, V] = np.linalg.svd(Y)
+    [Q, R] = np.linalg.qr(Z, mode="reduced")
+    Y = np.transpose(Q).dot(A)
+    [UY, S, V] = np.linalg.svd(Y, full_matrices=0)
     U = np.dot(Q,  UY)
     return [U, S, V]
-
