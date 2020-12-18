f10786309d44aaa2a60a0185745b08195ff7629c
Arturo Ortiz San Miguel <65433626+arturoortizx@users.noreply.github.com>
2020-12-18 04:20:41 -0400

    Update SVD_Plot.py

--- SVD_Plot.py
+++ SVD_Plot.py
@@ -1,4 +1,5 @@
 from PR_Senators import *
+from ControversialBills import *
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -21,9 +22,8 @@ def plot_svd(year, U):
     plt.title("SVD Analysis " + str(year))
     plt.show()
 
-def plot_pc(year, U, d):
-    A = d @ U
-
+def plot_pc(year, U_S): # U_S is a list of the form [U, S]
+    A = U_S[0] @ np.diag(U_S[1])
     t = pd.DataFrame({"X": A[:, 0], "Y": A[:, 1], "Party": np.array(parties)})
     groups = t.groupby("Party")
     for name, group in groups:
@@ -47,11 +47,11 @@ plot_err("2017-2020", myData)
 plt.savefig("2017-2020 error.png")
 plt.close()
 
-plot_svd("2017-2020", u_ret(myData))
+plot_svd("2017-2020", u_sig(myData)[0])
 plt.savefig("2017-2020 SVD.png")
 plt.close()
 
-plot_pc("2017-2020", myData, u_ret(myData))
+plot_pc("2017-2020", u_sig(myData))
 plt.savefig("2017-2020 PCA.png")
 plt.close()
 
@@ -61,13 +61,42 @@ for i in range(4):
     myData = read_txt(4 * i + 1, 4 * i + 4)
 
     plot_err(i + 2017, myData)
-    plt.savefig(str(i + 2017) + "error.png")
+    plt.savefig(str(i + 2017) + " error.png")
     plt.close()
 
-    plot_svd(i + 2017, u_ret(myData))
-    plt.savefig(str(i + 2017) + "SVD.png")
+    plot_svd(i + 2017, u_sig(myData)[0])
+    plt.savefig(str(i + 2017) + " SVD.png")
     plt.close()
 
-    plot_pc(i + 2017, myData, u_ret(myData))
-    plt.savefig(str(i + 2017) + "PCA.png")
+    plot_pc(i + 2017, u_sig(myData))
+    plt.savefig(str(i + 2017) + " PCA.png")
+    plt.close()
+
+myData = cont_bills(1, 16)
+
+plot_err("2017-2020 *", myData)
+plt.savefig("2017-2020 controversial error.png")
+plt.close()
+
+plot_svd("2017-2020 *", u_sig(myData)[0])
+plt.savefig("2017-2020 controversial SVD.png")
+plt.close()
+
+plot_pc("2017-2020 *", u_sig(myData))
+plt.savefig("2017-2020 controversial PCA.png")
+plt.close()
+
+for i in range(4):
+    myData = cont_bills(4 * i + 1, 4 * i + 4)
+
+    plot_err(str(i + 2017) + "*", myData)
+    plt.savefig(str(i + 2017) + " controversial error.png")
+    plt.close()
+
+    plot_svd(str(i + 2017) + "*", u_sig(myData)[0])
+    plt.savefig(str(i + 2017) + " controversial SVD.png")
+    plt.close()
+
+    plot_pc(str(i + 2017) + "*", u_sig(myData))
+    plt.savefig(str(i + 2017) + " controversial PCA.png")
     plt.close()
