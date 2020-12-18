08c88e20da0258d27ee234e95d5004fdc31818f1
Arturo Ortiz San Miguel <65433626+arturoortizx@users.noreply.github.com>
2020-12-18 04:22:12 -0400

    Add files via upload

---
+++ ControversialBills.py
@@ -0,0 +1,27 @@
+from PR_Senators import *
+
+def count_ones(col):
+    ones = 0
+    for i in col:
+        if i == 1:
+            ones = ones + 1
+    return ones
+
+def cont_bills(x, y):
+    myData = read_txt(x, y)
+
+    cont = 0
+    for i in range(np.shape(myData)[1]):
+        if count_ones(myData[:, i]) not in range(5, 25):
+           cont = cont + 1
+
+    A = np.zeros([30, cont])
+    j = 0
+    for i in range(np.shape(myData)[1]):
+        if count_ones(myData[:, i]) not in range(5, 25):
+            A[:, j] = myData[:, i]
+            j = j + 1
+
+    return A
+
+
