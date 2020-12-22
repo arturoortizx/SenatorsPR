6562f055681c95099d081a5f66d9eab33d8c8f74
Arturo Ortiz San Miguel <65433626+arturoortizx@users.noreply.github.com>
2020-12-22 18:54:26 -0400

    Update ControversialBills.py

--- ControversialBills.py
+++ ControversialBills.py
@@ -24,4 +24,22 @@ def cont_bills(x, y):
 
     return A
 
+def not_cont_bills(x, y):
+    myData = read_txt(x, y)
+
+    cont = 0
+    for i in range(np.shape(myData)[1]):
+        if count_ones(myData[:, i]) not in range(5, 25):
+           cont = cont + 1
+
+    A = np.zeros([30, cont])
+    print(cont)
+    j = 0
+    for i in range(np.shape(myData)[1]):
+        if count_ones(myData[:, i]) not in range(5, 25):
+            A[:, j] = myData[:, i]
+            j = j + 1
+
+    return A
+
 
